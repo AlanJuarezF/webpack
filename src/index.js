a057a089fe6ecdf9995629dc09fd61084e039331
Alan Juarez <alanwlfx@gmail.com>
2023-04-05 19:58:54 -0500

    contenido a empaquetar

---
+++ src/index.js
@@ -0,0 +1,37 @@
+
+const $links = document.getElementById('links');
+const $name = document.querySelector('h1');
+
+const data = {
+    name: "Alan Juarez Fragoso",
+    nickname: "AlanWLFx",
+    description: "...",
+    avatar: "...",
+    social: [
+      {
+        name: "faceebok",
+        url: "https://twitter.com/Alanwlfx",
+        username: "",
+      },
+      {
+        name: "instagram",
+        url: "https://www.instagram.com/alaan_wlf/",
+        username: "",
+      },
+    ],
+    links: [
+      {
+        name: "blog",
+        url: "https://example.com/",
+        color: "red",
+        emoji: "📖",
+      },
+      {
+        name: "podcast",
+        url: "https://example.com/",
+        color: "yellow",
+        emoji: "💬",
+      },
+    ],
+    footer: "Made with Love on Mexico",
+  };
